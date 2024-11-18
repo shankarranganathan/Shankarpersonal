f962dd948fff4a8d624c3ca12fff9cfb4b39087b
shankarranganathan <shankarmech100@gmail.com>
2024-11-19 05:01:34 +0530

    my desire

---
+++ src/org/personal/Personal.java
@@ -0,0 +1,22 @@
+package org.personal;
+
+public class Personal {
+	private void myname() {
+		System.out.println("shankar");
+
+	}
+	private void mywifename() {
+		System.out.println("priyanka");
+	}
+	private void childName() {
+		System.out.println("jai"+">>>"+"sai");
+	}
+public static void main(String[] args) {
+	Personal p=new Personal();
+	p.myname();
+	p.mywifename();
+	p.childName();
+}
+	
+
+}
